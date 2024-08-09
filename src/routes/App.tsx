69591b97434573bc33064f545c3c9286b1477456
JasonBMa <JasonBMa@users.noreply.github.com>
2024-08-08 21:24:33 -0700

    Was finally able to get the user access token

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,68 +1,74 @@
-import { useState } from 'react'
-import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+//import { useState } from 'react'
+import { Container, Row, Button } from "react-bootstrap";
+//import { useNavigate } from "react-router-dom";
 import './App.css'
 
 
-function App() {
-  const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-  const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
-  const [userAccessToken, setAccessToken] = useState("");
-  const navigate = useNavigate();
+const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+  //const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
 
-  const redirectUri = 'http://localhost:5173/SignedIn';
-  const generateRandomString = (length) => {
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const values = crypto.getRandomValues(new Uint8Array(length));
-    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-  }
-  
-  let codeVerifier = generateRandomString(64);
-  console.log("Code Is " + codeVerifier)
-  const sha256 = async (plain) => {
-    const encoder = new TextEncoder()
-    const data = encoder.encode(plain)
-    return window.crypto.subtle.digest('SHA-256', data)
-  }
+const redirectUri = 'http://localhost:5173/SignedIn';
 
-  const base64encode = (input) => {
-    return btoa(String.fromCharCode(...new Uint8Array(input)))
-      .replace(/=/g, '')
+function generateCodeVerifier(length: number) {
+  let text = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function generateCodeChallenge(codeVerifier: string) {
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest('SHA-256', data);
+  return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
       .replace(/\+/g, '-')
-      .replace(/\//g, '_');
+      .replace(/\//g, '_')
+      .replace(/=+$/, '');
+}
+
+const logInWithSpotify = async () => {
+  let codeVerifier = generateCodeVerifier(64);
+  const codeChallenge = await generateCodeChallenge(codeVerifier);;
+  window.localStorage.setItem('code_verifier', codeVerifier);
+
+  const scope = 'user-read-private user-read-email';
+  const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+  const params =  {
+    response_type: 'code',
+    client_id: clientId,
+    scope,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge,
+    redirect_uri: redirectUri,
   }
+  console.log(redirectUri)
+  authUrl.search = new URLSearchParams(params).toString();
+  window.location.href = authUrl.toString();
+}
 
-  const hashed = sha256(codeVerifier)
-  const codeChallenge = base64encode(hashed);
-  // generated in the previous step
-  const logInWithSpotify = () => {
-
-    const scope = 'user-read-private user-read-email';
-    const authUrl = new URL("https://accounts.spotify.com/authorize")
-  
-    window.localStorage.setItem('code_verifier', codeVerifier);
-    console.log(codeVerifier)
-    const params =  {
-      response_type: 'code',
-      client_id: clientId,
-      scope,
-      code_challenge_method: 'S256',
-      code_challenge: codeChallenge,
-      redirect_uri: redirectUri,
-    }
-    console.log(redirectUri)
-    authUrl.search = new URLSearchParams(params).toString();
-    window.location.href = authUrl.toString();
-
-    const urlParams = new URLSearchParams(window.location.search);
-    //getToken(urlParams.get('code'));
-    //navigate("/SignedIn", { state: { userAccessToken: userAccessToken }});
-  }
-  
+function App() {
   return (
     <>
-      <h1><span className="title-spot">Spot</span>Me</h1>
-      <Button className="spotify-themeify-btn" onClick={logInWithSpotify}>Log In With Spotify</Button>
+      <Container
+        style={{
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center"
+        }}
+      >
+        <Row
+          style={{
+            justifyContent: "around"
+          }}
+        >
+          <h1><span className="title-spot">Spot</span>Me</h1>
+          <Button className="spotify-themeify-btn" onClick={logInWithSpotify}>Log In With Spotify</Button>
+          
+        </Row>
+      </Container>
     </>
   )
 }
--- src/routes/signedIn.tsx
+++ src/routes/signedIn.tsx
@@ -1,69 +1,61 @@
 import { useState, useEffect } from 'react'
 import { FormControl, InputGroup, Container, Button, Row, Card } from "react-bootstrap";
-import { useLocation } from 'react-router-dom';
 import './App.css'
 
 function SignedIn() {
   const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-  const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+  //const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [userAccessToken, setUserAccessToken] = useState("");
   const [albums, setAlbums] = useState([]);
 
-  const location = useLocation();
-  const data = location.state;
-
   const redirectUri = 'http://localhost:5173/SignedIn';
 
-  const getToken = async (code) => {
-
-    // stored in the previous step
-    let codeVerifier = localStorage.getItem('code_verifier');
-    console.log(codeVerifier);
-    const payload = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        client_id: clientId,
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: redirectUri,
-        code_verifier: codeVerifier,
-      }),
-    }
-  
-    const body = await fetch("https://accounts.spotify.com/api/token", payload);
-    const response = await body.json();
-  
-    setUserAccessToken(response.access_token);
-  }
-  const urlParams = new URLSearchParams(window.location.search);
-  getToken(urlParams.get('code'));
-  console.log(userAccessToken)
-
   useEffect(() => {
-    let authParams = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body:
-        "grant_type=client_credentials&client_id=" +
-        clientId +
-        "&client_secret=" +
-        clientSecret,
-    };
+    // let authParams = {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/x-www-form-urlencoded",
+    //   },
+    //   body:
+    //     "grant_type=client_credentials&client_id=" +
+    //     clientId +
+    //     "&client_secret=" +
+    //     clientSecret,
+    // };
   
-    fetch("https://accounts.spotify.com/api/token", authParams)
-      .then((result) => result.json())
-      .then((data) => {
-        setAccessToken(data.access_token);
-      });
+    // fetch("https://accounts.spotify.com/api/token", authParams)
+    //   .then((result) => result.json())
+    //   .then((data) => {
+    //     setAccessToken(data.access_token);
+    //   });
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+    console.log("Code: " + code);
+    getAccessToken(clientId, code);    
   }, []);
-  
+
+  async function getAccessToken(clientId: string, code: string){
+    const verifier = localStorage.getItem("code_verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", redirectUri);
+    params.append("code_verifier", verifier!);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
+    const { access_token } = await result.json();
+    setUserAccessToken(access_token);
+}
+
   async function search() {
     let artistParams = {
       method: "GET",
@@ -96,69 +88,80 @@ function SignedIn() {
       });
   }
 
+
   return (
     <>
-      <h1><span className="title-spot">Spot</span>Me</h1>
-      <Container>
-        <InputGroup>
-          <FormControl
-            placeholder="Search For Artist"
-            type="input"
-            aria-label="Search for an Artist"
-            onKeyDown={(event) => {
-              if (event.key === "Enter") {
-                search();
-              }
-            }} // search function
-            onChange={(event) => setSearchInput(event.target.value)} // setSearch
-            style={{
-              width: "300px",
-              height: "35px",
-              borderWidth: "0px",
-              borderStyle: "solid",
-              borderRadius: "5px",
-              marginRight: "10px",
-              paddingLeft: "10px",
-            }}
-          />
-
-          <Button onClick={search}>Search</Button>
-        </InputGroup>
-        <Row
+    <Container
+      style={{
+        width: "100vw",
+        justifyContent: "center"
+      }}
+    >
+      <Row>
+        <a style={{textDecoration:'none'}}><h1><span className="title-spot">Spot</span>Me</h1></a>
+      </Row>
+      <Row>
+      <InputGroup>
+        <FormControl
+          placeholder="Search For Artist"
+          type="input"
+          aria-label="Search for an Artist"
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              search();
+            }
+          }} // search function
+          onChange={(event) => setSearchInput(event.target.value)} // setSearch
           style={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            justifyContent: "space-around",
-            alignContent: "center",
-            marginTop: '10px',
+            width: "300px",
+            height: "35px",
+            borderWidth: "0px",
+            borderStyle: "solid",
+            borderRadius: "5px",
+            marginRight: "10px",
+            paddingLeft: "10px",
           }}
-        >
-          {
-            albums.map((album) => {
-              return <Card>
-                <Card.Img
-                  width={200}
-                  src={album.images[0].url}
-                  style={{borderRadius: '4%',}}
-                />
-                <Card.Body style={{color:'white'}}>
-                  <Card.Title style={{
-                    whiteSpace: 'wrap',
-                    fontWeight: 'bold',
-                    maxWidth: '200px',
-                    fontSize: '18px',
-                    marginTop: '10px',
-                    }}
-                  >{album.name}</Card.Title>
+        />
 
-                  <Card.Text>{album.release_date}</Card.Text>
-                </Card.Body>
-              </Card>
-            })
-          }
-        </Row>
-      </Container>
+        <Button onClick={search}>Search</Button>
+      </InputGroup>
+
+      </Row>
+      <Row
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          alignContent: "center",
+          marginTop: '10px',
+        }}
+      >
+        {
+          albums.map((album) => {
+            return <Card>
+              <Card.Img
+                width={200}
+                src={album.images[0].url}
+                style={{borderRadius: '4%',}}
+              />
+              <Card.Body style={{color:'white'}}>
+                <Card.Title style={{
+                  whiteSpace: 'wrap',
+                  fontWeight: 'bold',
+                  maxWidth: '200px',
+                  fontSize: '18px',
+                  marginTop: '10px',
+                  }}
+                >{album.name}</Card.Title>
+
+                <Card.Text>{album.release_date}</Card.Text>
+              </Card.Body>
+            </Card>
+          })
+        }
+      </Row>
+    </Container>
     </>
   )
 }
