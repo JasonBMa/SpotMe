9179380dbb1d3eb1ca16b34f77302b02e00542f1
JasonBMa <JasonBMa@users.noreply.github.com>
2024-08-22 14:38:57 -0700

    cleaned up callback, removing dead code

--- src/routes/Callback.tsx
+++ src/routes/Callback.tsx
@@ -1,14 +1,9 @@
-import { useState, useEffect } from 'react'
-import { FormControl, InputGroup, Container, Button, Row, Card } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import './App.css'
 
 function Callback() {
   const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
   //const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
-  const [userAccessToken, setUserAccessToken] = useState("");
-  const [searchInput, setSearchInput] = useState("");
-  const [albums, setAlbums] = useState([]);
   const navigate = useNavigate();
 
   const redirectUri = 'http://localhost:5173/Callback';
@@ -16,7 +11,7 @@ function Callback() {
   const params = new URLSearchParams(window.location.search);
   const code = params.get("code"); //gets code from url returned from Spotify OAuth
   console.log("Code: " + code);
-  getAccessToken(clientId, code)
+  getAccessToken(clientId, code!)
     .then(() => {
       navigate("/Home");
     });
@@ -46,50 +41,6 @@ function Callback() {
 
   return (
     <>
-    <Container
-      style={{
-        width: "100vw",
-        justifyContent: "center"
-      }}
-    >
-      <Row>
-        <a style={{textDecoration:'none'}}><h1><span className="title-spot">Spot</span>Me</h1></a>
-      </Row>
-      <Row
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "space-around",
-          alignContent: "center",
-          marginTop: '10px',
-        }}
-      >
-        {
-          albums.map((album) => {
-            return <Card>
-              <Card.Img
-                width={200}
-                src={album.images[0].url}
-                style={{borderRadius: '4%',}}
-              />
-              <Card.Body style={{color:'white'}}>
-                <Card.Title style={{
-                  whiteSpace: 'wrap',
-                  fontWeight: 'bold',
-                  maxWidth: '200px',
-                  fontSize: '18px',
-                  marginTop: '10px',
-                  }}
-                >{album.name}</Card.Title>
-
-                <Card.Text>{album.release_date}</Card.Text>
-              </Card.Body>
-            </Card>
-          })
-        }
-      </Row>
-    </Container>
     </>
   )
 }
